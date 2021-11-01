f2c1ac929746de19540f09f379502bbc791b3fd2
Lucas lopes <lucasomena@handtalk.me>
2021-11-01 16:46:05 -0300

    Update new types of food

---
+++ Snake 2D IAs/Assets/FastFood.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+//First Food Class
+public class FastFood : MonoBehaviour
+{
+    //Adding Box Collider (gridArea)
+    public BoxCollider2D gridArea;
+    
+    //Method to respawn the food
+    private void Start()
+    {
+        RandomizePosition();
+    }
+
+    //method to set the random position
+    private void RandomizePosition()
+    {
+        Bounds bounds = this.gridArea.bounds;
+
+        float x = Random.Range(bounds.min.y, bounds.max.y);
+        float y = Random.Range(bounds.min.y, bounds.max.y);
+
+        this.transform.position = new Vector3(Mathf.Round(x), Mathf.Round(y), 0.0f);
+    }
+
+    //method to trigger the action when one of the snakes collide with the food
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            RandomizePosition();
+        }
+    }
+}
