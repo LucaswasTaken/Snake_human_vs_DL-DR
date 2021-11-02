51d1a24d9d9d8c3d3e4efc321470574ce812e88c
Lucas lopes <lucasomena@handtalk.me>
2021-11-02 07:52:42 -0300

    Letting only one changeble food

--- Snake 2D IAs/Assets/FastFood.cs
+++ Snake 2D IAs/Assets/FastFood.cs
@@ -6,10 +6,14 @@ public class FastFood : MonoBehaviour
     //Adding Box Collider (gridArea)
     public BoxCollider2D gridArea;
     
+    //Spriter Render
+    public SpriteRenderer sr;
+    
     //Method to respawn the food
     private void Start()
     {
         RandomizePosition();
+
     }
 
     //method to set the random position
@@ -19,7 +23,19 @@ public class FastFood : MonoBehaviour
 
         float x = Random.Range(bounds.min.y, bounds.max.y);
         float y = Random.Range(bounds.min.y, bounds.max.y);
-
+        
+        float foodType = Random.Range(0, 2);
+        sr = GetComponent<SpriteRenderer>();
+        if (foodType==1)
+        {
+            this.transform.gameObject.tag = "fastFood";
+            this.sr.color = Color.red;
+        }
+        else
+        {
+            this.transform.gameObject.tag = "lifeFood";
+            this.sr.color = Color.blue;
+        }
         this.transform.position = new Vector3(Mathf.Round(x), Mathf.Round(y), 0.0f);
     }
 
---
+++ Snake 2D IAs/Temp/ScriptUpdater/1034605408/1239460082_FastFood.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+//First Food Class
+public class FastFood : MonoBehaviour
+{
+    //Adding Box Collider (gridArea)
+    public BoxCollider2D gridArea;
+    
+    //Method to respawn the food
+    private void Start()
+    {
+        RandomizePosition();
+
+    }
+
+    //method to set the random position
+    private void RandomizePosition()
+    {
+        Bounds bounds = this.gridArea.bounds;
+
+        float x = Random.Range(bounds.min.y, bounds.max.y);
+        float y = Random.Range(bounds.min.y, bounds.max.y);
+        
+        float foodType = Random.Range(0, 2);
+        if (foodType==1)
+        {
+            this.transform.gameObject.tag = "fastFood";
+            this.GetComponent<Renderer>().material.color = Color.red;
+        }
+        else
+        {
+            this.transform.gameObject.tag = "lifeFood";
+            this.GetComponent<Renderer>().material.color = Color.blue;
+        }
+        this.transform.position = new Vector3(Mathf.Round(x), Mathf.Round(y), 0.0f);
+    }
+
+    //method to trigger the action when one of the snakes collide with the food
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            RandomizePosition();
+        }
+
+        else if (other.tag == "snakeBody")
+        {
+            RandomizePosition();
+        }
+
+        else if (other.tag == "Obstacle")
+        {
+            RandomizePosition();
+        }
+    }
+}
